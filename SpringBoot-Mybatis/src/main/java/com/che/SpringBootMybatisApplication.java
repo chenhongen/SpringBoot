3763ce93fd3a6bc95e7b860a8287b492f549977c
che <red.model@163.com>
2017-12-23 19:30:44 +0800

    add mybatis

---
+++ SpringBoot-Mybatis/src/main/java/com/che/SpringBootMybatisApplication.java
@@ -0,0 +1,14 @@
+package com.che;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.che.dao")
+public class SpringBootMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisApplication.class, args);
+	}
+}
